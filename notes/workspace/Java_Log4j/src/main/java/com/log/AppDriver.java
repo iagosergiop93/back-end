2b90c84481846f8282e60c76448f209f0b89e1bc
iagosergiop@gmail.com <iagosergiop@gmail.com>
2019-11-16 15:31:28 -0500

    Log4J

---
+++ notes/workspace/Java_Log4j/src/main/java/com/log/AppDriver.java
@@ -0,0 +1,29 @@
+package com.log;
+
+import java.util.LinkedList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AppDriver {
+	
+	private static final Logger logger = LogManager.getLogger();
+	
+	private static final Logger loggerTest = LogManager.getLogger("test");
+	
+	public static void main(String[] args) {
+		logger.trace("Trace message");
+		logger.debug("Debug message");
+		logger.info("Info Message!");
+	    logger.warn("Warn Message!");
+	    logger.error("Error Message!");
+	    logger.fatal("Fatal Message!");
+	    
+	    System.out.println("\n");
+	    
+	    loggerTest.debug("Debug msg");
+	    loggerTest.info("Info msg");
+	    
+	}
+
+}
