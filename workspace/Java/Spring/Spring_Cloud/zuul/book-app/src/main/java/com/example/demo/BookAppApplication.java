ca9c3202e330cbb1edc1a2800635b39822d37211
iagosergiop93 <iagosergiop93@gmail.com>
2020-09-15 12:07:23 -0400

    Config Server for Microservices

--- workspace/Java/Spring/Spring_Cloud/zuul/book-app/src/main/java/com/example/demo/BookAppApplication.java
+++ workspace/Java/Spring/Spring_Cloud/zuul/book-app/src/main/java/com/example/demo/BookAppApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,3 +33,17 @@ public class BookAppApplication {
 	}
 	
 }
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+  @Value("${message:Hello default}")
+  private String message;
+
+  @RequestMapping("/message")
+  String getMessage() {
+    return this.message;
+  }
+}
+
